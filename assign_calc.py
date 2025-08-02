f44ed549d90db7f549f09ec677268397c38ec6c7
COLLINS MAKITA <123198923+cmakita-coder@users.noreply.github.com>
2025-08-02 11:49:25 +0300

    Add files via upload

---
+++ assign_calc.py
@@ -0,0 +1,25 @@
+
+
+# user input
+num1 = float(input("Enter the first number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+# Perform calculation based on the operation
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Error: Invalid operation.")
